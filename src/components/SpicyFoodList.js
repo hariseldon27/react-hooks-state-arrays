6e551e795591aa033ae332b6eaeafc3c2a40cb65
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-02 13:25:36 -0800

    commit tree 1a64e4f2c66e8626cb50a34aa3c273362284497b

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,20 +3,65 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All")
+  const foodsToDisplay = foods.filter((food) => { 
+    if(filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  })
+
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArry = [ ...foods, newFood]
+    // console.log(newFood);
+    setFoods(newFoodArry)
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  // this one removes li items
+  // function handleLIClick(id) {
+  //   const newFoodArry = foods.filter((food) => food.id !== id)
+  //   setFoods(newFoodArry)
+  // }
+
+  //this one increments the heat level in the arry
+  function handleLIClick(id){
+    const newFoodArry = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1
+        }
+      } else {
+        return food
+      }
+    })
+    setFoods(newFoodArry)
+  }
+
+  function handleFilter(event){
+      setFilterBy(event.target.value)
+  }
+  
+  
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLIClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
   return (
     <div>
+      <select name="filter" onChange={handleFilter}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
